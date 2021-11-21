942294c6b03c9a1d72d7d59a196588b605c6ced2
Matthias Hecht <dev@hecht.space>
2021-11-21 23:21:57 +0100

    build: updating configs

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,12 +4,12 @@ import { startTracking, Tracker, TrackingOptions } from 'gut-js';
 export const skipTracking = Symbol('skip');
 export type SkipTracking = typeof skipTracking;
 
-export function useTracking(tracker: Tracker, optionsOrSkip?: TrackingOptions | SkipTracking): void {
+export const useTracking = (tracker: Tracker, optionsOrSkip?: TrackingOptions | SkipTracking) => {
   useEffect(() => {
     if (optionsOrSkip !== skipTracking) {
       return startTracking(tracker, optionsOrSkip);
     }
   }, [tracker, optionsOrSkip]);
-}
+};
 
 export { Dataset, Stop, Tracker, TrackingContext, TrackingOptions } from 'gut-js';
