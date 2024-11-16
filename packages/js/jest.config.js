47647e4ffbc08c4149bf33837a3daa50bd2ec25d
Matthias Hecht <matthias@hecht.space>
2024-11-16 22:00:03 +0100

    chore: update react dependencies

--- packages/js/jest.config.js
+++ packages/js/jest.config.js
@@ -1,17 +1,12 @@
 /* eslint-disable import/no-unused-modules */
 
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['ts', 'js'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/', '/examples/'],
   testRegex: ['/__tests__/.*\\.(ts|js)$', '/*.test\\.(ts|js)$'],
   transform: {
-    '\\.ts$': 'ts-jest',
+    '\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
 };
--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,17 +1,12 @@
 /* eslint-disable import/no-unused-modules */
 
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/', '/examples/'],
   testRegex: ['/__tests__/.*\\.(ts|tsx|js|jsx)$', '/*.test\\.(ts|tsx|js|jsx)$'],
   transform: {
-    '\\.(ts|tsx)$': 'ts-jest',
+    '\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
 };
