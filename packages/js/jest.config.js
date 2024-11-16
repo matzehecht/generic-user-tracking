3a3c172580e320075ae5ada10c2c5c7d3859fbf4
Matthias Hecht <matthias@hecht.space>
2024-11-16 20:57:15 +0100

    chore: update linting deps

--- packages/js/jest.config.js
+++ packages/js/jest.config.js
@@ -1,15 +1,17 @@
+/* eslint-disable import/no-unused-modules */
+
 module.exports = {
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'js'],
-  moduleDirectories: ['node_modules', 'src'],
-  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/', '/examples/'],
-  transform: {
-    '\\.ts$': 'ts-jest',
-  },
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.json',
     },
   },
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'js'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/', '/examples/'],
   testRegex: ['/__tests__/.*\\.(ts|js)$', '/*.test\\.(ts|js)$'],
+  transform: {
+    '\\.ts$': 'ts-jest',
+  },
 };
--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,15 +1,17 @@
+/* eslint-disable import/no-unused-modules */
+
 module.exports = {
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  moduleDirectories: ['node_modules', 'src'],
-  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/', '/examples/'],
-  transform: {
-    '\\.(ts|tsx)$': 'ts-jest',
-  },
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.json',
     },
   },
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/', '/examples/'],
   testRegex: ['/__tests__/.*\\.(ts|tsx|js|jsx)$', '/*.test\\.(ts|tsx|js|jsx)$'],
+  transform: {
+    '\\.(ts|tsx)$': 'ts-jest',
+  },
 };
